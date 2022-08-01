a8e925d627972c5d452ff30489efa5d218df8613
shultztom <shultzthomask@sau.edu>
2022-08-01 16:50:52 -0500

    added stats

--- src/features/WeightStats/index.js
+++ src/features/WeightStats/index.js
@@ -3,16 +3,23 @@ import { useSelector } from "react-redux";
 import {selectToken, selectUser} from "../Login/loginSlice";
 import { get } from 'lodash';
 import axios from "axios";
-import {Grid, Typography} from "@mui/material";
+import {Grid, Typography, Item} from "@mui/material";
 import {convertMetricToImperial} from "../../utils/convert";
 
 function WeightChart () {
     const user = useSelector(selectUser);
     const token = useSelector(selectToken);
     const [weightData, setWeightData] = useState([]);
+    const [stats, setStats] = useState(null);
+
+    const round = (value, precision) => {
+        let multiplier = Math.pow(10, precision || 0);
+        return Math.round(value * multiplier) / multiplier;
+
+    }
 
     const fetchData = async (time) => {
-        const axiosConfig = {
+        const axiosConfigLastEntry = {
             method: 'GET',
             url: `${process.env.REACT_APP_WEIGHT_TRACKER_API}/entry/username/${user}/last`,
             headers: {
@@ -20,10 +27,21 @@ function WeightChart () {
             }
         }
 
+        const axiosConfigStats = {
+            method: 'GET',
+            url: `${process.env.REACT_APP_WEIGHT_TRACKER_API}/stats/all/${user}`,
+            headers: {
+                'x-auth-token': token
+            }
+        }
+
         try {
-            const results = await axios(axiosConfig);
-            const data = get(results, 'data', []);
-            setWeightData(convertMetricToImperial(data));
+            const lastResults = await axios(axiosConfigLastEntry);
+            const lastData = get(lastResults, 'data', []);
+            setWeightData(convertMetricToImperial(lastData));
+            const statsResults = await axios(axiosConfigStats);
+            const statsData = get(statsResults, 'data', null);
+            setStats(statsData);
         } catch (e) {
             console.log(e.message);
         }
@@ -39,8 +57,23 @@ function WeightChart () {
               direction="column"
               alignItems="center"
               mt={4}>
-            <Typography variant='h3'>{Number(get(weightData, 'weight', '')).toFixed(1)} lbs</Typography>
-
+            <Grid item xs={12}>
+                <Typography variant='h3'>{round(get(weightData, 'weight', 0), 1)} lbs</Typography>
+            </Grid>
+            <Grid container mt={4}>
+                <Grid item xs={4}>
+                    <Typography align={"center"}>BMR</Typography>
+                    <Typography align={"center"}>{round(get(stats, "BMR", 0), 1)}</Typography>
+                </Grid>
+                <Grid item xs={4}>
+                    <Typography align={"center"}>TDEE</Typography>
+                    <Typography align={"center"}>{round(get(stats, "TDEE", 0), 1)}</Typography>
+                </Grid>
+                <Grid item xs={4}>
+                    <Typography align={"center"}>BMI</Typography>
+                    <Typography align={"center"}>{round(get(stats, "BMI", 0) ,1)}</Typography>
+                </Grid>
+            </Grid>
         </Grid>
     );
 }
