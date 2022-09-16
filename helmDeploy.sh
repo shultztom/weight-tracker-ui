7753494d0ff9b4f476ce9bf972a4e1e88ad5e1ab
shultztom <shultzthomask@sau.edu>
2022-09-16 15:59:57 -0500

    worked on script to use version matching package.json

--- helmDeploy.sh
+++ helmDeploy.sh
@@ -1,4 +1,6 @@
+PACKAGE_VERSION=$(node -p "require('./package.json').version")
+
 gcloud auth print-access-token | helm registry login -u oauth2accesstoken \
 --password-stdin https://us-central1-docker.pkg.dev
 
-helm upgrade --install weight-tracker-ui oci://us-central1-docker.pkg.dev/shultzlab/shultzlab-helm/http-deploy --version 0.1.0 -f .helm/deploy/values.yaml
+helm upgrade --install weight-tracker-ui oci://us-central1-docker.pkg.dev/shultzlab/shultzlab-helm/http-deploy --version 0.1.0 -f .helm/deploy/values.yaml --set image.tag=$PACKAGE_VERSION 
